5f55f69b41c899d4b51560f2748a9c0ef80ee92f
hoya <hoya@192.168.219.101>
2020-03-30 23:29:56 +0900

    4,5단 추

--- src/Re_Gugudan.java
+++ src/Re_Gugudan.java
@@ -1,28 +1,79 @@
 
+import java.util.Scanner;
+
 public class Re_Gugudan {
 	public static void main(String[] args) {
 		//2단 - 계산 및 출력
-		System.out.println("2단");
-		System.out.println(2*1);
-		System.out.println(2*2);
-		System.out.println(2*3);
-		System.out.println(2*4);
-		System.out.println(2*5);
-		System.out.println(2*6);
-		System.out.println(2*7);
-		System.out.println(2*8);
-		System.out.println(2*9);
+//		System.out.println("2단");
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
 		
 		//3단 - 계산 및 출력
-		System.out.println("3단");
-		System.out.println(3*1);
-		System.out.println(3*2);
-		System.out.println(3*3);
-		System.out.println(3*4);
-		System.out.println(3*5);
-		System.out.println(3*6);
-		System.out.println(3*7);
-		System.out.println(3*8);
-		System.out.println(3*9);
+//		System.out.println("3단");
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
+		
+		//4단 - 결과를 변수로 출력
+		int number = 4;
+		
+		System.out.println("4단");
+		int result = number * 1;
+		System.out.println(result);
+		result = number * 2;
+		System.out.println(result);
+		result = number * 3;
+		System.out.println(result);
+		result = number * 4;
+		System.out.println(result);
+		result = number * 5;
+		System.out.println(result);
+		result = number * 6;
+		System.out.println(result);
+		result = number * 7;
+		System.out.println(result);
+		result = number * 8;
+		System.out.println(result);
+		result = number * 9;
+		System.out.println(result);
+		
+		//5단 - 사용자에게 숫자 값을 입력 받는 방법
+//		System.out.println("구구단 중 출력할 단은? : ");
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        System.out.println("number : " + number);
+//
+//        int result = number * 1;
+//        System.out.println(result);
+//        result = number * 2;
+//        System.out.println(result);
+//        result = number * 3;
+//        System.out.println(result);
+//        result = number * 4;
+//        System.out.println(result);
+//        result = number * 5;
+//        System.out.println(result);
+//        result = number * 6;
+//        System.out.println(result);
+//        result = number * 7;
+//        System.out.println(result);
+//        result = number * 8;
+//        System.out.println(result);
+//        result = number * 9;
+//        System.out.println(result);
+		
 	}
 }
