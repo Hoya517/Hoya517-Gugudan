559759b845dc2b7283643a7fa59cdb6ebde22fea
hoya <hoya@192.168.219.101>
2020-03-31 00:36:50 +0900

    이중반복문 추

--- src/Re_Gugudan.java
+++ src/Re_Gugudan.java
@@ -2,13 +2,22 @@
 
 public class Re_Gugudan {
 	public static void main(String[] args) {
+		//배열 + 이중반복문
 		int[] result = new int[9];
-		for(int i=0; i<result.length; i++) {
-			result[i] = 2 * (i+1);
+		for(int j=2; j<10; j++) {
+			for(int i=0; i<result.length; i++) {
+				result[i] = j*(i+1);
+				System.out.println(result[i]);
+			}
 		}
-		for(int j=0; j<result.length; j++) {
-			System.out.println(result[j]);
-		}
+		
+		//배열 + for반복
+//		for(int i=0; i<result.length; i++) {
+//			result[i] = 2 * (i+1);
+//		}
+//		for(int j=0; j<result.length; j++) {
+//			System.out.println(result[j]);
+//		}
 		
 		
 	}
