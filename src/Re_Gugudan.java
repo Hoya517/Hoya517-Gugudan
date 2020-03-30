83a9d800dd0b4784e92050715f857dab49cc81c6
hoya <hoya@192.168.219.101>
2020-03-31 00:32:19 +0900

    배열로 구구단 구현하

--- src/Re_Gugudan.java
+++ src/Re_Gugudan.java
@@ -3,13 +3,13 @@
 public class Re_Gugudan {
 	public static void main(String[] args) {
 		int[] result = new int[9];
-		
-		for(int j=2; j<10; j++) {
-			for(int i=0; i<result.length;i++) {
-				result[i] = j * (i+1);
-				System.out.println(result[i]);
-			}
+		for(int i=0; i<result.length; i++) {
+			result[i] = 2 * (i+1);
 		}
+		for(int j=0; j<result.length; j++) {
+			System.out.println(result[j]);
+		}
+		
 		
 	}
 }
